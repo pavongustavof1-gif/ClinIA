0c37d1a52aa37fe6947b5cf4baed12ddc756b38a
pavongustavof1-gif <pavongustavof1@gmail.com>
2025-12-22 13:16:56 -0500

    Add audio input and success message to app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+st.title("ClinIA Asistente de Notas en Español 🇲🇽")
+st.write("Haz clic en el micrófono para empezar a hablar.")
+
+
 audio_value = st.audio_input("Record a voice message")
 if audio_value:
     st.audio(audio_value)
+    st.success("Grabación capturada exitosamente.")
+
